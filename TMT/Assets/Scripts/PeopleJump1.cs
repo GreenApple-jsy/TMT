02dcaf95a6a36cf548c17294756fbe49f8af82ca
GA <greenapplej16@gmail.com>
2025-12-07 23:20:31 +0900

    Create PeopleJump1.cs

---
+++ TMT/Assets/Scripts/PeopleJump1.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeopleJump1 : MonoBehaviour
+{
+    public float RandomJumpSpeed;
+    public bool IsGoingUp;
+    public bool NumeratorGoingOn;
+
+    void Start()
+    {
+        RandomJumpSpeed = Random.Range(1f, 5.5f);
+        IsGoingUp = true;
+        NumeratorGoingOn = false;
+    }
+
+    void Update()
+    {
+        if ((IsGoingUp == true) && (NumeratorGoingOn == false))
+        {
+            NumeratorGoingOn = true;
+            StartCoroutine(GoUp());
+        }
+        else if ((IsGoingUp == false) && (NumeratorGoingOn == false))
+        {
+            NumeratorGoingOn = true;
+            StartCoroutine(GoDown());
+        }
+    }
+
+    public IEnumerator GoUp()
+    {
+        while (this.transform.position.y <= -40f)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, (this.transform.position.y + (RandomJumpSpeed * 0.2f)), this.transform.position.z);
+            yield return 0.1f;
+        }
+        yield return new WaitForSeconds(0.15f);
+        IsGoingUp = false;
+        NumeratorGoingOn = false;
+    }
+
+    public IEnumerator GoDown()
+    {
+        while (this.transform.position.y >= -48.3f)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, (this.transform.position.y - (RandomJumpSpeed * 0.2f)), this.transform.position.z);
+            yield return 0.1f;
+        }
+        yield return new WaitForSeconds(0.07f);
+        IsGoingUp = true;
+        NumeratorGoingOn = false;
+    }
+}
